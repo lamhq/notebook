b73d1c08eecad876ebe8516bc83ce242ce006f44
Lam <daibanglam@gmail.com>
2026-04-03 21:39:13 +0700

    feat: verify jwt token

--- apps/api/src/api/main.ts
+++ apps/api/src/api/main.ts
@@ -2,7 +2,7 @@ import { getNestApp } from './app';
 
 async function bootstrap() {
   const app = await getNestApp();
-  await app.listen(3000);
+  await app.listen(4069);
 }
 
 bootstrap().catch((error: unknown) => {
