4aacdbd3daaa4ed324bb8c559bc7343cc743483e
Lam <daibanglam@gmail.com>
2026-04-17 09:32:50 +0700

    feat: set up automatic versioning

--- .github/skills/terraform/deployment.md
+++ .github/skills/terraform/deployment.md
@@ -29,3 +29,9 @@ Follow these steps to deploy the infrastructure to an environment:
    ```bash
    terraform apply -var-file=config/${env}.tfvars
    ```
+
+9. In GCP, update Google Auth Platform / Clients:
+   - Set **Authorized JavaScript origins** to `https://<cognito-domain>`
+   - Set **Authorized redirect URIs** to `https://<cognito-domain>/oauth2/idpresponse`
+
+10. In GCP, update Google Auth Platform / Branding, add Cognito domain to **Authorized domains**
--- apps/infra/modules/cognito-user/main.tf
+++ apps/infra/modules/cognito-user/main.tf
@@ -12,12 +12,15 @@ terraform {
 }
 
 resource "random_password" "admin_temporary_password" {
-  length           = 16
-  lower            = true
-  upper            = true
-  numeric          = true
-  special          = true
-  override_special = "!@#$%&*"
+  length      = 16
+  lower       = true
+  upper       = true
+  numeric     = true
+  special     = true
+  min_lower   = 3
+  min_upper   = 3
+  min_numeric = 3
+  min_special = 1
 }
 
 resource "aws_cognito_user" "admin_user" {
